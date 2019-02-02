67199088a34aa8cb9c9884a1792caa9cad7f8baa
lidongyang <15650785172@163.com>
2019-02-02 19:22:47 +0800

    轮播

---
+++ main.js
@@ -0,0 +1,37 @@
+var allButtons = $('#buttons > button')
+
+for(let i=0;i<allButtons.length;i++){
+  $(allButtons[i]).on('click',function(x){
+    var index = $(x.currentTarget).index()
+    var p= index*(-300)
+    $('#images').css({
+      transform: 'translate('+ p+ 'px)'
+    })
+    n = index
+    activeButton(allButtons.eq(n))
+  })
+}
+
+var n=0
+var size = allButtons.length
+playSlide(n%size)
+
+var timerId = setTimer()
+$('.window').on('mouseenter',function(){
+  window.clearInterval(timerId)
+})
+$('.window').on('mouseleave',function(){
+  timerId = setTimer()
+})
+function activeButton($button){
+  $button.addClass('grey').siblings('.grey').removeClass('grey')
+}
+function playSlide(index){
+  allButtons.eq(index).trigger('click')
+}
+function setTimer(){
+  return setInterval(()=>{
+    n += 1
+    playSlide(n%size)
+  },2000)
+}
